39b47854e825c45f634256ceff4f1a7b29fc75b7
deveshrawat <devsrawt@gmail.com>
2020-11-12 00:49:47 +0530

    final commit

--- todos-backend/index.js
+++ todos-backend/index.js
@@ -9,7 +9,7 @@ const start = async () => {
       throw new Error("Mongo uri must be defined.");
    }
    try {
-      await mongoose.connect(process.env.MONGO_ATLAS, {
+      await mongoose.connect(process.env.MONGO_URI, {
          useNewUrlParser: true,
          useUnifiedTopology: true,
          useCreateIndex: true,
